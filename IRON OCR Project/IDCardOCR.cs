521cf876e945e15b6779ac9148cae716f0a6ca1d
Ahmad Ashraf <a.ashraf@sanasofteg.com>
2023-03-19 10:05:41 +0200

    Add All Project Files

---
+++ IRON OCR Project/IDCardOCR.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IronOcr;
+using IronSoftware.Drawing;
+
+namespace IRON_OCR_Project
+{
+    public class IDCardOCR
+    {
+        private int x_axis;
+        private int y_axis;
+        private int width;
+        private int height;
+        private string imagePath;
+        private int scale;
+        private string saveFileWithName;
+        IronTesseract Ocr;
+        public IDCardOCR(string imagePath, int scale, string saveFileWithName,int xaxis , int yaxis ,int width , int height)
+        {
+            this.x_axis = xaxis;
+            this.y_axis = yaxis; 
+            this.width = width;
+            this.height = height;
+            this.imagePath = imagePath;
+            this.scale = scale;
+            this.saveFileWithName = saveFileWithName;
+            this.Ocr = new IronTesseract();
+        }
+
+        private CropRectangle CropRect { 
+            get{
+                return new IronSoftware.Drawing.CropRectangle(x_axis, y_axis, width, height);
+            }
+        }
+
+        private void settingUp_OCR()
+        {
+            this.Ocr.Language = OcrLanguage.ArabicAlphabetBest;
+            this.Ocr.AddSecondaryLanguage(OcrLanguage.ArabicAlphabetBest);
+            this.Ocr.UseCustomTesseractLanguageFile(@"tessdata_arabic-master\ara-Amiri.traineddata");
+            this.Ocr.UseCustomTesseractLanguageFile(@"tessdata_arabic-master\ara-Amiri-layer.traineddata");
+            this.Ocr.Configuration.BlackListCharacters = "1234567890\"'؟!÷×؛~ْ^%،<>+=*::!@#$%^&*()_+QWERTYUIOPASDFGHJKL;ZXCVBNM,??qwertyuiopasdfghjklzxcvbnm~`$#^*_}{][|\\@¢©«»°±·×‑–—‘’“”•…′″€™←↑→↓↔⇄⇒∅∼≅≈≠≤≥≪≫⌁⌘○◔◑◕●☐☑☒☕☮☯☺♡⚓✓✰";
+            this.Ocr.Configuration.PageSegmentationMode = TesseractPageSegmentationMode.Auto;
+            this.Ocr.Configuration.ReadBarCodes = false;
+
+        }
+
+        public void print_ID_Info( Predicate<bool> useFilter)
+        {
+            settingUp_OCR();
+
+            using (var ocrInput = new OcrInput())
+            {
+                CropRectangle rec = this.CropRect;
+
+                Bitmap bm = (Bitmap)Bitmap.FromFile(imagePath);
+
+                ocrInput.AddImage(bm, rec);
+
+                if (useFilter(false))
+                {
+
+                }
+
+                //ocrInput.Erode();
+                ocrInput.Binarize();
+                ocrInput.DeNoise();
+                ocrInput.SelectTextColor(IronSoftware.Drawing.Color.Black);
+                
+                var newImage = ocrInput.StampCropRectangleAndSaveAs(rec,IronSoftware.Drawing.Color.Red, saveFileWithName);
+                ocrInput.Scale(scale);
+
+                var ocrResult = Ocr.Read(ocrInput);
+
+                Console.WriteLine(ocrResult.Text);
+                Console.WriteLine("-----------------------------------------------------------------------");
+                ocrResult.SaveAsSearchablePdf($"../../../OCR_Result/{saveFileWithName}.pdf");
+                ocrResult.SaveAsTextFile($"../../../OCR_Result/{saveFileWithName}.txt");
+
+            }
+
+        }
+    }
+}
---
+++ IRON OCR Project/Program.cs
@@ -0,0 +1,68 @@
+﻿
+namespace IRON_OCR_Project
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var face_IdCard = new IDCardOCR("../../../Images/ID-Face.jpg",24, "arabic_Face_rec", 900, 300, 1003, 800);
+            // scale = 24
+
+            face_IdCard.print_ID_Info();
+
+            var back_IdCard = new IDCardOCR("../../../Images/ID-Back.jpg",80, "arabic_Back_rec", 600, 80, 1025, 580); 
+            //scale = 63
+
+            back_IdCard.print_ID_Info();
+        }
+    }
+}
+
+
+
+//using (var ocrInput = new OcrInput("../../../Images/ID-Face.jpg"))
+//{
+//    //ocrInput.DeNoise();
+//    //ocrInput.Despeckle();
+
+//    ocrInput.Erode();
+//    //ocrInput.Open();
+//    //ocrInput.ToGrayScale();
+//    ocrInput.Binarize();
+//    //ocrInput.Sharpen();
+
+//    ocrInput.SelectTextColor(Color.Black);
+//    var rec = new CropRectangle(900,300,1200,800);
+//    //ocrInput.Scale(130);
+//    var newImage = ocrInput.StampCropRectangleAndSaveAs(rec,Color.Red);
+//    ocrInput.Scale(130);//125
+
+//    var ocrResult = Ocr.Read(ocrInput);
+
+//    Console.WriteLine(ocrResult.Text);
+//    Console.WriteLine("-----------------------------------------------------------------------");
+//    Console.WriteLine(newImage[0]);
+//    ocrResult.SaveAsSearchablePdf("../../../OCR_Result/arabic_Face.pdf");
+//    ocrResult.SaveAsTextFile("../../../OCR_Result/arabic_Face.txt");
+
+//}
+
+
+//using (var ocrInput = new OcrInput("../../../Images/ID-Back.jpg"))
+//{
+//    ocrInput.DeNoise();
+//    //ocrInput.Despeckle();
+//    ocrInput.Binarize();
+//    ocrInput.Sharpen();
+//    ocrInput.Erode();
+//    ocrInput.Scale(112,true); //130
+
+
+//    var ocrResult = Ocr.Read(ocrInput);
+
+//    Console.WriteLine(ocrResult.Text);
+//    ocrResult.SaveAsSearchablePdf("../../../OCR_Result/arabic_back.pdf");
+//    ocrResult.SaveAsTextFile("../../../OCR_Result/arabic_back.txt");
+//    //Check if the file exists
+
+//}
